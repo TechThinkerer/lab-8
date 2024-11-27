bdbba2cb5a8df216e406e56e5c66cb8eaca44b3e
TechThinkerer <233609@students.au.edu.pk>
2024-11-27 14:59:35 +0500

    Add files via upload

---
+++ Customer Preview/Customer Preview/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Customer_Preview
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            string country = textBox2.Text;
+            string gender = radioButton3.Checked ? "male" : "female";
+            string hobby = checkBox3.Checked ? "reading" : (checkBox4.Checked ? "painting" : "none");
+            string status = radioButton5.Checked ? "married" : "unmarried";
+            Form2 objPreview = new Form2();
+            objPreview.SetValues(name, country, gender, hobby, status);
+            objPreview.Show();
+
+
+
+            string connectionString = "Server=AUMC-LAB3-06\\SQLEXPRESS;Database=customerdb;Trusted_Connection=True;";
+            using (Server = AUMC - LAB3 - 06\\SQLEXPRESS; Database = customerdb; Trusted_Connection = True connection = new "Server=AUMC-LAB3-06\\SQLEXPRESS;Database=customerdb;Trusted_Connection=True;"; (connectionString))
+        {
+                connection.Open();
+                string query = "INSERT INTO YourTableName (Name, Country, Gender, Hobby, Status) VALUES (@Name, @Country, @Gender, @Hobby, @Status)";
+                using (dataGridView1 command = new dataGridView1(query, connection))
+                {
+                    command.Parameters.AddWithValue("@Name", name);
+                    command.Parameters.AddWithValue("@Country", country);
+                    command.Parameters.AddWithValue("@Gender", gender);
+                    command.Parameters.AddWithValue("@Hobby", hobby);
+                    command.Parameters.AddWithValue("@Status", status);
+
+                    command.ExecuteNonQuery();
+                }
+                LoadDataIntoGrid();
+
+            }
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void LoadDataIntoGrid()
+        {
+            string connectionString = "Server=AUMC-LAB3-06\\SQLEXPRESS;Database=customerdb;Trusted_Connection=True;";
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM YourTableName"; 
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(query, connection))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+                        dataGridView1.DataSource = dataTable;
+                    }
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                MessageBox.Show("Database error: " + sqlEx.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+    }
+}
